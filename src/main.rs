366a59e6523502102ad5cff0893fdab945d9a18a
Zachary Sohovich <zachsohovich@thebriga.de>
2019-08-02 19:11:01 -0700

    added sendgrid contact update endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,53 @@
-extern crate actix_web;
+extern crate dotenv;
 
-use actix_web::Result;
+use actix_web::client::Client;
+use actix_web::{middleware, web, App, Error, HttpResponse, HttpServer};
+use futures::Future;
+use serde::{Deserialize, Serialize};
+use dotenv::dotenv;
+use std::env;
 
-fn index() -> Result<String> {
-  Ok(format!("Hello World!"))
+#[derive(Debug, Serialize, Deserialize)]
+struct SignupEmail {
+  email: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Contact {
+  contacts: Vec<SignupEmail>
+}
+
+fn signup(item: web::Json<SignupEmail>) -> impl Future<Item = HttpResponse, Error = Error> {
+  Client::new()
+    .put("https://api.sendgrid.com/v3/marketing/contacts")
+    .bearer_auth(env::var("SENDGRID_API_KEY").unwrap())
+    .send_json(&Contact {
+      contacts: vec![item.0]
+    })
+    .map_err(Error::from)
+    .and_then(|mut response| {
+      response
+        .body()
+        .from_err()
+        .and_then(|body| {
+          Ok(HttpResponse::Ok().body(body))
+        })
+    })
 }
 
 fn main() {
-  use actix_web::{web, App, HttpServer};
+  dotenv().ok();
+  std::env::set_var("RUST_LOG", "actix_web=info");
+  env_logger::init();
 
-  HttpServer::new(|| App::new().route("/", web::get().to(index)))
-    .bind("127.0.0.1:8080")
-    .unwrap()
-    .run()
-    .unwrap()
+  HttpServer::new(|| {
+    App::new()
+      .wrap(middleware::Logger::default())
+      .data(web::JsonConfig::default().limit(4096))
+      .service(web::resource("/signup").route(web::post().to_async(signup)))
+  })
+  .bind("127.0.0.1:8080")
+  .unwrap()
+  .run()
+  .unwrap()
 }
